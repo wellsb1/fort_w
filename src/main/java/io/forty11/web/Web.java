6ed5d6f005be3ebaee28a407974a292b01bda34f
Tim Collins <tim@rocketpartners.io>
2018-05-14 17:15:08 -0400

    rev'd to 0.1.8 and changed Lang to J

--- src/main/java/io/forty11/web/Web.java
+++ src/main/java/io/forty11/web/Web.java
@@ -65,7 +65,6 @@ import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.ssl.TrustStrategy;
 
 import io.forty11.j.J;
-import io.forty11.j.api.Lang;
 import io.forty11.j.api.Streams;
 import io.forty11.utils.Executor;
 
@@ -991,7 +990,7 @@ public class Web
 
       public void setUrl(String url)
       {
-         if (!Lang.empty(url))
+         if (!J.empty(url))
          {
             url = url.trim();
             url = url.replaceAll(" ", "%20");
@@ -999,12 +998,12 @@ public class Web
 
          this.url = url;
 
-         if (Lang.empty(fileName))
+         if (J.empty(fileName))
          {
             try
             {
                fileName = new URL(url).getFile();
-               if (Lang.empty(fileName))
+               if (J.empty(fileName))
                   fileName = null;
             }
             catch (Exception ex)
